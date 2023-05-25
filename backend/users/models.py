5cbc0b20848a306aa31cd403055acbb88f1cd343
Puneet Punia <puneet.punia.nga@crowdbotics.com>
2023-05-25 17:37:50 +0530

    changed user model and admin

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
-
+import uuid
 
 class User(AbstractUser):
     # WARNING!
