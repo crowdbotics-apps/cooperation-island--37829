27a37699006a540a52512179bc9f215636570939
Puneet Punia <puneet.punia.nga@crowdbotics.com>
2023-10-04 16:12:58 +0530

    Heroku changes for celery

--- backend/cooperation_island_37829/settings.py
+++ backend/cooperation_island_37829/settings.py
@@ -327,6 +327,6 @@ if GS_BUCKET_NAME:
 FCM_DJANGO_SETTINGS = {"FCM_SERVER_KEY": env.str("FCM_SERVER_KEY", "Your FCM Server Key")}
 
 
-CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_BROKER_URL = env.str("REDIS_URL", "redis://redis:6379/0")
 CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 
